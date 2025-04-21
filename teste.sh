3461e25d56cbc529866fedbc501f0212e4246dcd
Djalma Henrique <76885802+DjalmaHenri@users.noreply.github.com>
2025-04-20 23:32:00 -0300

    Add files via upload

---
+++ teste.sh
@@ -0,0 +1,23 @@
+#!/bash/bash
+
+pkg update
+
+pkg update
+
+pkg install git
+
+pkg install proot-distro
+
+pkg install net-tools
+
+pkg install neofetch
+
+pkg instal openssh
+
+sshd
+
+passwd
+
+proot-distro install debian
+
+neofetch
